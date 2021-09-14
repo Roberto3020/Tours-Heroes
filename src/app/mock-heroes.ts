6e111c50a69ba3911f3f2302f6afe9961934cb5d
Roberto Orcasita <robertorcasitta@gmail.com>
2021-09-14 10:02:16 -0500

    empezando

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES :Hero[] = [
+    {id:11 , name:'Dr nice'},
+    {id:12, name:'Narco'},
+    {id:13, name:'Bombasto'},
+    {id:14, name:'Calerista'},
+    {id:15, name:'Magneta'},
+    {id:16, name:'N'},
+    {id:17, name:'Narco'}
+]
