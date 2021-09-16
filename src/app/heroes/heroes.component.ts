2b78feec2348385fef428f8a36dccba2dda74df3
Roberto Orcasita <robertorcasitta@gmail.com>
2021-09-15 21:22:37 -0500

    Punto 4 Confirmado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,19 +10,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
   hero:Hero = {
     id:1,
     name:'Windstrom'
   };
-  heroes = HEROES;
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('heroescomponent: Selected Hero id=${hero.id}')
   }
   
-  constructor() { }
-
+  constructor( private heroService:HeroService,
+    private messageService:MessageService) { }
+getHeroes():void{
+  this.heroService.getHeroes()
+  .subscribe(heroes => this.heroes = heroes);
+}
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
